16a9829d95736734fa8aa7c4e58b3c1259868731
Yong Zhang <allennyzhang@gmail.com>
2020-04-05 23:00:32 +0800

    add sorting for all of the fields

--- src/components/flightResult.jsx
+++ src/components/flightResult.jsx
@@ -4,7 +4,7 @@ import {
     createStyles,
     Table, TableBody, TableCell,
     TableHead, TableRow,
-    Typography, withStyles
+    Typography, withStyles, TableSortLabel
 } from '@material-ui/core'
 import { Actions, } from '../redux';
 import { tableStyles } from '../assets';
@@ -28,19 +28,25 @@ class FlightResultImp extends React.PureComponent {
         pageSize: 10,
         pageIndex: 0,
         totalHits: 0,
+        orderByField: "departure",
+        sortDirection: "desc",
     };
 
     getpageRecords = () => {
-        let pageRecords = [];
         const allRecords = this.props.flightState.flights;
         this.setState({ totalHits: allRecords.length }, () => {
-            const { pageSize, pageIndex, totalHits } = this.state;
-            const firstIndex = pageIndex * pageSize;
-            const lastIndex = ((pageIndex + 1) * pageSize) > totalHits ? totalHits : ((pageIndex + 1) * pageSize);
-            pageRecords = allRecords.slice(firstIndex, lastIndex);
-
-            this.props.setPageRecords(pageRecords);
+            this.setPageRecords(allRecords);
         });
+    }
+
+    setPageRecords = (allRecords) => {
+        const totalHits = allRecords.length;
+        const { pageSize, pageIndex, } = this.state;
+        const firstIndex = pageIndex * pageSize;
+        const lastIndex = ((pageIndex + 1) * pageSize) > totalHits ? totalHits : ((pageIndex + 1) * pageSize);
+        const pageRecords = allRecords.slice(firstIndex, lastIndex);
+
+        this.props.setPageRecords(pageRecords);
     }
 
     handlePageChange = page => {
@@ -51,6 +57,53 @@ class FlightResultImp extends React.PureComponent {
     handlePageSizeChange = value => {
         console.log(`pageIndex:${value};pageSize:${value}`)
         this.setState({ pageIndex: 0, pageSize: value }, () => { this.getpageRecords(); });
+    }
+
+    handleSorting = (orderByField, changeDirection = true) => {
+        const sortValue = this.state.sortDirection;
+        const sortDirection = changeDirection ? sortValue === 'asc' ? 'desc' : 'asc' : sortValue;
+        this.setState({ orderByField, sortDirection }, () => { this.sortResults(); });
+    }
+
+    sortLabelDirection = () => {
+        return this.state.sortDirection ? this.state.sortDirection : undefined;
+    }
+
+    sortResults = () => {
+        let sortedRecords = this.props.flightState.pageRecords;;
+        const flights = this.props.flightState.flights;
+        switch (this.state.orderByField) {
+            case 'departure':
+                sortedRecords = flights.slice().sort((a, b) => this.sortString(a.departure, b.departure));
+                break;
+            case 'arrival':
+                sortedRecords = flights.slice().sort((a, b) => this.sortString(a.arrival, b.arrival));
+                break;
+            case 'departureTime':
+                sortedRecords = flights.slice().sort((a, b) => this.sortString(a.departureTime, b.departureTime));
+                break;
+            case 'arrivalTime':
+                sortedRecords = flights.slice().sort((a, b) => this.sortString(a.arrivalTime, b.arrivalTime));
+                break;
+            default:
+                return sortedRecords;
+        }
+        if (this.state.sortDirection === 'asc') {
+            sortedRecords = sortedRecords.reverse();
+        }
+        this.setPageRecords(sortedRecords);
+    }
+
+    sortString = (a, b) => {
+        const nameA = a.toLowerCase();
+        const nameB = b.toLowerCase();
+        if (nameA < nameB) {
+            return 1;
+        }
+        if (nameA > nameB) {
+            return -1;
+        }
+        return 0;
     }
 
     render() {
@@ -64,20 +117,40 @@ class FlightResultImp extends React.PureComponent {
                     <TableHead>
                         <TableRow>
                             <TableCell className={clsx(classes.tableCell, classes.tableHeadCell)}>
-                                Departure
-                                    </TableCell>
+                                <TableSortLabel
+                                    active={this.state.orderByField === 'departure'}
+                                    direction={this.sortLabelDirection()}
+                                    onClick={() => this.handleSorting('departure')}>
+                                    Departure
+								</TableSortLabel>
+                            </TableCell>
                             <TableCell className={clsx(classes.tableCell, classes.tableHeadCell)}>
-                                Arrival
-							        </TableCell>
+                                <TableSortLabel
+                                    active={this.state.orderByField === 'arrival'}
+                                    direction={this.sortLabelDirection()}
+                                    onClick={() => this.handleSorting('arrival')}>
+                                    Arrival
+								</TableSortLabel>
+                            </TableCell>
                             <TableCell className={clsx(classes.tableCell, classes.tableHeadCell)}>
-                                Departure Time
-							        </TableCell>
+                                <TableSortLabel
+                                    active={this.state.orderByField === 'departureTime'}
+                                    direction={this.sortLabelDirection()}
+                                    onClick={() => this.handleSorting('departureTime')}>
+                                    Departure Time
+								</TableSortLabel>
+                            </TableCell>
                             <TableCell className={clsx(classes.tableCell, classes.tableHeadCell)}>
-                                Arrival Time
-							        </TableCell>
+                                <TableSortLabel
+                                    active={this.state.orderByField === 'arrivalTime'}
+                                    direction={this.sortLabelDirection()}
+                                    onClick={() => this.handleSorting('arrivalTime')}>
+                                    Arrival Time
+								</TableSortLabel>
+                            </TableCell>
                             <TableCell className={clsx(classes.tableCell, classes.tableHeadCell)}>
                                 Class
-							        </TableCell>
+							</TableCell>
                         </TableRow>
                     </TableHead>
                     {pageRecords.length > 0 && !isLoading &&
